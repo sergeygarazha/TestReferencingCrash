279cbac0b99a60e93c8491838913656399dc26ec
Sergey Garazha <garazha.sergey@gmail.com>
2023-02-10 13:53:36 +0300

    use objc_sync_enter for fix

--- TestReferencingCrash/ViewModel.swift
+++ TestReferencingCrash/ViewModel.swift
@@ -5,13 +5,15 @@ private var cancellables: Set<AnyCancellable> = []
 
 class ViewModel {
     func test() {
-        for _ in (0...1000) {
+        for _ in (0...10000) {
             DispatchQueue(label: "test", qos: .default)
             //DispatchQueue.main
                 .async {
-                Just("Test").sink(receiveValue: { value in
-                    print(value)
-                }).store(in: &cancellables)
+                    objc_sync_enter(cancellables)
+                        Just("Test").sink(receiveValue: { value in
+                            print(value)
+                        }).store(in: &cancellables)
+                    objc_sync_exit(cancellables)
             }
         }
     }
