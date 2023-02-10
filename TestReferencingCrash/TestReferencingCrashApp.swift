888a60c5e3cbe8004c89f17577971c8d5d46dc28
Sergey Garazha <garazha.sergey@gmail.com>
2023-02-10 13:39:30 +0300

    Initial Commit

---
+++ TestReferencingCrash/TestReferencingCrashApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestReferencingCrashApp.swift
+//  TestReferencingCrash
+//
+//  Created by Sergey Garazha on 10.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestReferencingCrashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
