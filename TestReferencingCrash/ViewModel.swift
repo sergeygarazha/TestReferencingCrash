ff719ab07275b504c2bda8df98759f4bc62dfa90
Sergey Garazha <garazha.sergey@gmail.com>
2023-02-10 14:06:03 +0300

    use os_unfair_lock

--- TestReferencingCrash/ViewModel.swift
+++ TestReferencingCrash/ViewModel.swift
@@ -1,7 +1,27 @@
 import Foundation
 import Combine
 
-private var lock = NSLock()
+// Read http://www.russbishop.net/the-law for more information on why this is necessary
+final class UnfairLock {
+    private var _lock: UnsafeMutablePointer<os_unfair_lock>
+
+    init() {
+        _lock = UnsafeMutablePointer<os_unfair_lock>.allocate(capacity: 1)
+        _lock.initialize(to: os_unfair_lock())
+    }
+
+    deinit {
+        _lock.deallocate()
+    }
+
+    func locked<ReturnValue>(_ f: () throws -> ReturnValue) rethrows -> ReturnValue {
+        os_unfair_lock_lock(_lock)
+        defer { os_unfair_lock_unlock(_lock) }
+        return try f()
+    }
+}
+
+private let lock = UnfairLock()
 private var cancellables: Set<AnyCancellable> = []
 
 class ViewModel {
@@ -14,9 +34,9 @@ class ViewModel {
                         print(value)
                     })
 
-                    lock.lock()
-                    cancellable.store(in: &cancellables)
-                    lock.unlock()
+                    lock.locked {
+                        cancellable.store(in: &cancellables)
+                    }
             }
         }
     }
